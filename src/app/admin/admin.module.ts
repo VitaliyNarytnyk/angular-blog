e5e98196ca3ec7a6b9bcd2d87ed3158b7e351edd
VitaliyNarytnyk <vitaliy.narytnyk@gmail.com>
2022-07-22 21:36:08 +0300

    created SearchPipe for filtering list of posts

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from "@angular/core";
 import { AdminRoutingModule } from "./admin-routing.module";
 import { AdminLayoutComponent } from "./shared/components/admin-layout/admin-layout.component";
 import { AdminComponent } from './admin.component';
-import { AuthService } from "./shared/services/auth.service";
 import { SharedModule } from "app/shared/shared.module";
 import { AuthGuard } from "./shared/services/auth.guard";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
@@ -12,14 +11,14 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 @NgModule({
 	declarations: [
 		AdminLayoutComponent,
-		AdminComponent
+		AdminComponent,
 	],
 	imports: [
 		CommonModule,
 		AdminRoutingModule,
 		SharedModule,
 		FormsModule,
-		ReactiveFormsModule
+		ReactiveFormsModule,
 	],
 	exports: [AdminComponent],
 	providers: [AuthGuard]
--- src/app/admin/dashboard-page/dashboard-page.module.ts
+++ src/app/admin/dashboard-page/dashboard-page.module.ts
@@ -3,12 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardPageComponent } from './dashboard-page.component';
 import { DashboardPageRoutingModule } from './dashboard-page-routing.module';
+import { FormsModule } from '@angular/forms';
+import { SearchPipe } from '../shared/pipes/search.pipe';
 
 @NgModule({
-  declarations: [DashboardPageComponent],
+  declarations: [
+    DashboardPageComponent,
+    SearchPipe
+  ],
   imports: [
     CommonModule,
-    DashboardPageRoutingModule
+    DashboardPageRoutingModule,
+    FormsModule
   ],
   exports: [DashboardPageComponent]
 })
