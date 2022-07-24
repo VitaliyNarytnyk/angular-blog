0d462f14e624311a59deed6ea9d6aef666904347
VitaliyNarytnyk <vitaliy.narytnyk@gmail.com>
2022-07-24 17:57:53 +0300

    added language localisation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { registerLocaleData } from '@angular/common';
+import ruUaLocale from '@angular/common/locales/ru-UA';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule, Provider } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -7,6 +9,8 @@ import { AppComponent } from './app.component';
 import { AuthInterceptor } from './shared/auth.interceptor';
 import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
 import { SharedModule } from './shared/shared.module';
+
+registerLocaleData(ruUaLocale, 'ru')
 
 const INTERCEPTOR_PROVIDER: Provider = {
   provide: HTTP_INTERCEPTORS,
