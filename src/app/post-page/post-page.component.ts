0928f665a9631c1982c8ff24bec7964e38e7f683
VitaliyNarytnyk <vitaliy.narytnyk@gmail.com>
2022-07-24 18:38:44 +0300

    added display of posts on post-page

--- src/app/post-page/post-page.component.ts
+++ src/app/post-page/post-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Post } from 'app/shared/interfaces';
+import { PostsService } from 'app/shared/posts.service';
+import { Observable, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-post-page',
@@ -7,9 +11,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostPageComponent implements OnInit {
 
-  constructor() { }
+  post$!: Observable<Post>
 
-  ngOnInit(): void {
+  constructor(
+    private route: ActivatedRoute,
+    private postsService: PostsService
+  ) { }
+
+  ngOnInit() {
+    this.post$ = this.route.params
+      .pipe(switchMap((params: Params) => {
+        return this.postsService.getById(params['id'])
+      }))
   }
 
 }
--- src/app/post-page/post-page.module.ts
+++ src/app/post-page/post-page.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { PostPageRoutingModule } from './post-page-routing.module';
 import { PostPageComponent } from './post-page.component';
+import { QuillModule } from 'ngx-quill';
 
 
 @NgModule({
   declarations: [PostPageComponent],
   imports: [
     CommonModule,
-    PostPageRoutingModule
+    PostPageRoutingModule,
+    QuillModule
   ],
   exports: [PostPageComponent]
 })
